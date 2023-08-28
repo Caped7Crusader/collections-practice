939ceb04aadc88383813535e6a1ac57de81e6456
Caped7Crusader <kkishoresenthilkumar@gmail.com>
2023-08-28 11:51:28 +0530

    added field without main

--- src/git/demo.java
+++ src/git/demo.java
@@ -2,11 +2,7 @@ package git;
 
 public class demo {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-		int id;
-		String name;
-	}
+	private int id;
+	private String name;
 
 }
